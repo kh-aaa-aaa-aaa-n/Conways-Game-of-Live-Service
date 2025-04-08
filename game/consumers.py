1d03b28f873bfccc838429c7704e0d032c143b0c
Logan Wohlwend <logan.wohlwend@gmail.com>
2025-04-08 14:51:30 -0500

    Added basic user authentication check and move timeout foundations to consumers.py

--- game/consumers.py
+++ game/consumers.py
@@ -3,6 +3,12 @@ from channels.generic.websocket import AsyncWebsocketConsumer
 
 class GameConsumer(AsyncWebsocketConsumer):
     async def connect(self):
+        # Check if user is authenticated
+        self.user = self.scope["user"]
+        if self.user.is_anonymous:
+            await self.close()
+            return
+        
         await self.channel_layer.group_add("game_room", self.channel_name)
         await self.accept()
 
@@ -10,6 +16,16 @@ class GameConsumer(AsyncWebsocketConsumer):
         await self.channel_layer.group_discard("game_room", self.channel_name)
 
     async def receive(self, text_data):
+        ### TODO: Maybe implement something here for the move timeout
+        self.can_move = True
+        if not self.can_move:
+            await self.send(text_data = json.dumps({
+                "type": "error",
+                "message": "You must wait before making another move."
+            }))
+            return
+        ############################################################
+
         await self.channel_layer.group_send(
             "game_room",
             {
